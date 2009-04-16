7122923ba3049b0b06abc666f04603398cf163cd
Pascal.Andre <Pascal.Andre@e8ad57a4-18b6-11de-ac9f-1d9250355bca>
2009-04-16 15:51:05 +0000

    Added Properties

--- librarysim/Classes/Media.cs
+++ librarysim/Classes/Media.cs
@@ -6,7 +6,78 @@ namespace librarysim.Classes
 {
 	public class Media
 	{
-		
+    int mediaID;
+
+    public int MediaID
+    {
+      get { return mediaID; }
+      set { mediaID = value; }
+    }
+
+    int patronID;
+
+    public int PatronID
+    {
+      get { return patronID; }
+      set { patronID = value; }
+    }
+
+    string type;
+
+    public string Type
+    {
+      get { return type; }
+      set { type = value; }
+    }
+
+    string title;
+
+    public string Title
+    {
+      get { return title; }
+      set { title = value; }
+    }
+
+    string rating;
+
+    public string Rating
+    {
+      get { return rating; }
+      set { rating = value; }
+    }
+
+    string description;
+
+    public string Description
+    {
+      get { return description; }
+      set { description = value; }
+    }
+
+    DateTime checkedin;
+
+    public DateTime Checkedin
+    {
+      get { return checkedin; }
+      set { checkedin = value; }
+    }
+
+    DateTime checkedout;
+
+    public DateTime Checkedout
+    {
+      get { return checkedout; }
+      set { checkedout = value; }
+    }
+
+    bool reserved;
+
+    public bool Reserved
+    {
+      get { return reserved; }
+      set { reserved = value; }
+    }
+
 		public Media( DataRow mediaDR )
 		{
 		}
