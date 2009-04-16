e788c00864a9bd039526bce418ff2f779dc2dd65
Pascal.Andre <Pascal.Andre@e8ad57a4-18b6-11de-ac9f-1d9250355bca>
2009-04-16 23:47:57 +0000

    Properties

--- librarysim/Classes/Patrons.cs
+++ librarysim/Classes/Patrons.cs
@@ -6,10 +6,57 @@ namespace librarysim.Classes
 {
 	public class Patrons
 	{
-		
+    #region Variables
+    int patronID;
+    string name;
+    string phoneNumber;
+    string address;
+    string gender;
+    string dob;
+    #endregion
+
+    #region Properties
+    public int PatronID
+    {
+      get { return patronID; }
+    }
+
+    public string Name
+    {
+      get { return name; }
+    }
+
+    public string PhoneNumber
+    {
+      get { return phoneNumber; }
+    }
+
+    public string Address
+    {
+      get { return address; }
+    }
+
+    public string Gender
+    {
+      get { return gender; }
+    }
+
+    public string Dob
+    {
+      get { return dob; }
+    }
+    #endregion
+
+	
 		public Patrons( DataRow patronDR )
-		{
-		}
+    {
+      patronID = (int)patronDR["patronID"];
+      name = patronDR["name"].ToString();
+      phoneNumber = patronDR["phoneNumber"].ToString();
+      address = patronDR["address"].ToString();
+      gender = patronDR["gender"].ToString();
+      dob = patronDR["dob"].ToString();
+    }
 	}
 	
 	public class PatronsListViewItem : ListViewItem
