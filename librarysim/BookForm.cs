3637ec113fa471b94db7235fea72df0d5d71e7b9
Pascal.Andre <Pascal.Andre@e8ad57a4-18b6-11de-ac9f-1d9250355bca>
2009-04-20 21:29:47 +0000

    Added BookForm

---
+++ librarysim/BookForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using librarysim.Classes;
+
+namespace librarysim
+{
+  public partial class BookForm : Form
+  {
+    int bookID = 0;
+    
+    public BookForm()
+    {
+      InitializeComponent();
+      cmbType.SelectedIndex = 0;
+    }
+
+    public BookForm( Books b )
+    {
+      InitializeComponent();
+      bookID = b.BooksID;
+      txtPatronID.ReadOnly = false;
+      txtPatronID.Text = b.PatronID.ToString(); // todo
+      cmbType.SelectedIndex = (b.Type == "Adult") ? 1 : 0; // todo
+      txtTitle.Text = b.Title;
+      txtDescription.Text = b.Description;
+      dtpCheckedOut.Value = new DateTime(); // todo
+    }
+    
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
